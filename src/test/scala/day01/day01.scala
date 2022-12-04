7a4e6dc6c7b15acf4041488de85bdf56d887f73d
ilEnzio <westfury@gmail.com>
2022-12-04 14:00:59 -0500

    feat(Day4)

---
+++ src/test/scala/day01/day01.scala
@@ -0,0 +1,17 @@
+package day01
+
+import day01_Data.Day01Generators.genSnackData
+import org.scalacheck.Properties
+import puzzles.Day01
+
+object day01Test extends Properties("Day 1 Tests") {
+
+  // Snack test
+  // Elf Tests
+  //
+//  object Day01TestInterpreter extends Day01 {
+//    override def snackData = genSnackData.sample.get
+//  }
+//
+//  property("")
+}
